0db1f01f1e887e7fbc3cc626afc32af210aee70a
Altair-Mateus <taicoalencastro@gmail.com>
2022-07-13 23:33:34 -0300

    Adicionado Projeto OO-Polimorfismo

---
+++ POO Java/OO-Polimorfismo/src/ClasseMae.java
@@ -0,0 +1,12 @@
+public class ClasseMae {
+
+    void metodo1()
+    {
+        System.out.println("Método 1 da Classe Mãe");
+    }// Fim do método 1
+
+    void metodo2()
+    {
+        System.out.println("Método 2 da Classe Mãe");
+    }// Fim do metodo2
+}
