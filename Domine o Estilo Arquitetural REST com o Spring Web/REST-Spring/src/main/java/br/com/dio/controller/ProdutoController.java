331339954bcec38fe8183e6823c16b144b93c444
Altair-Mateus <taicoalencastro@gmail.com>
2022-07-26 23:36:55 -0300

    Endpoints REST e Exceptions Handlers

--- Domine o Estilo Arquitetural REST com o Spring Web/REST-Spring/src/main/java/br/com/dio/controller/ProdutoController.java
+++ Domine o Estilo Arquitetural REST com o Spring Web/REST-Spring/src/main/java/br/com/dio/controller/ProdutoController.java
@@ -1,10 +1,14 @@
 package br.com.dio.controller;
 
 import br.com.dio.entity.Produto;
+import br.com.dio.exception.ProductNullException;
+import br.com.dio.exception.ProductPriceException;
 import br.com.dio.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/produto")
@@ -14,11 +18,28 @@ public class ProdutoController {
     private ProdutoService service;
 
     @PostMapping(value = "/save")
-    public ResponseEntity<Produto> salvarProduto(@RequestBody Produto produto)
+    public ResponseEntity<Produto> salvarProduto(@RequestBody Produto produto) throws Exception
     {
         produto = service.save(produto);
 
         return ResponseEntity.ok().body(produto);
     }
+
+    @PostMapping(value = "/{id}")
+    public ResponseEntity<Produto> buscaProduto(@PathVariable Long id)
+    {
+        Produto produto = service.findById(id);
+
+        return ResponseEntity.ok().body(produto);
+    }
+
+    @PostMapping(value = "/busca-todos")
+    public ResponseEntity<List<Produto>> buscaTodosProdutos()
+    {
+        List<Produto> produtos = service.findAll();
+
+        return ResponseEntity.ok().body(produtos);
+    }
+
 
 }
--- Domine o Estilo Arquitetural REST com o Spring Web/REST-Spring/src/main/java/br/com/dio/service/ProdutoService.java
+++ Domine o Estilo Arquitetural REST com o Spring Web/REST-Spring/src/main/java/br/com/dio/service/ProdutoService.java
@@ -1,6 +1,8 @@
 package br.com.dio.service;
 
 import br.com.dio.entity.Produto;
+import br.com.dio.exception.ProductNullException;
+import br.com.dio.exception.ProductPriceException;
 import br.com.dio.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,7 +15,11 @@ public class ProdutoService {
     @Autowired
     private ProdutoRepository repository;
 
-    public Produto save(Produto produto)  {
+    public Produto save(Produto produto)  throws Exception{
+        if(produto.getNome() == null || produto.getPreco() == null)
+            throw new ProductNullException();
+        if(produto.getPreco() < 0)
+            throw new ProductPriceException();
         return repository.save(produto);
     }
 
