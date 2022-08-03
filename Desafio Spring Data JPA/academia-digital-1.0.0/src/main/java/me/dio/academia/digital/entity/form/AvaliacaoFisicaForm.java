af79ad2fa0d3f834d0ce39d7aed13132642f1529
Altair-Mateus <taicoalencastro@gmail.com>
2022-08-02 21:12:09 -0300

    Inserido o tratamento de validações nos forms

--- Desafio Spring Data JPA/academia-digital-1.0.0/src/main/java/me/dio/academia/digital/entity/form/AvaliacaoFisicaForm.java
+++ Desafio Spring Data JPA/academia-digital-1.0.0/src/main/java/me/dio/academia/digital/entity/form/AvaliacaoFisicaForm.java
@@ -3,16 +3,25 @@ package me.dio.academia.digital.entity.form;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import me.dio.academia.digital.entity.Aluno;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class AvaliacaoFisicaForm {
 
+  @NotNull(message = "Preencha o campo corretamente!")
+  @Positive(message = "O id do aluno deve ser positivo")
   private Long alunoId;
 
+  @NotNull(message = "Preencha o campo corretamente!")
+  @Positive(message = "O peso '${validatedValue}' deve ser positivo")
   private double peso;
 
+  @NotNull(message = "Preencha o campo corretamente!")
+  @Positive(message = "A altura '${validatedValue}' deve ser positivo")
   private double altura;
+
 }
